69d3acc25ee639bb8c0d9da073c5e5d5dd415ad1
vaibhav gehani <vgehani90@gmail.com>
2020-07-05 14:02:04 +0530

    model and dataset added

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask,request,abort,jsonify
 from flask_cors import CORS
+import numpy as np
+import pickle
 app = Flask(__name__)
 CORS(app)
-def doProcess(windSpeed, windDirection):
-    return ''
-
+model = pickle.load(open('xgbModel.pkl','rb'))
 @app.route('/')
 def index():
     return "Hello"
@@ -12,14 +12,13 @@ def index():
 @app.route('/predict',methods=['POST'])
 def predictValue():
     try:
-        global windSpeed
-        global windDirection
-        print(request.json)
         windSpeed = float(request.json['windSpeed'])
         windDirection = float(request.json['WindDirection'])
-        windGust = doProcess(windSpeed,windDirection)
-        print(windSpeed, windDirection, windGust)
-        return jsonify(output=200)
+        windGust = float(request.json['windGust']);
+        output = model.predict(np.array([[windSpeed,windDirection,windGust]]))
+        print(output)
+        activePower = output[0]
+        return jsonify(output=str(activePower))
     except Exception as e:
         print("error",e)
         abort(400)
